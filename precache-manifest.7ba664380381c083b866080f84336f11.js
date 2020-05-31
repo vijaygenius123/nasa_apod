4271da1d8d126ea4eb4b4a7ba5d1f0ab5fdfbfae
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-05-31 13:33:30 +0530

    Updates

---
+++ precache-manifest.7ba664380381c083b866080f84336f11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c96cef5963d3ef1b522dcd1eb9e3970",
+    "url": "/nasa_apod/index.html"
+  },
+  {
+    "revision": "f6c33c2b8adfbc5d8dda",
+    "url": "/nasa_apod/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "633aaa22358b58453f79",
+    "url": "/nasa_apod/static/js/2.2313dd4b.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/nasa_apod/static/js/2.2313dd4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c33c2b8adfbc5d8dda",
+    "url": "/nasa_apod/static/js/main.2c4c0a04.chunk.js"
+  },
+  {
+    "revision": "61634e0e2b43b5be1825",
+    "url": "/nasa_apod/static/js/runtime-main.38f9ecd3.js"
+  }
+]);
